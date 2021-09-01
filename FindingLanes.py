8ad61e570274e0e44b9fccabf59dee9cee14b7d2
Hakan Akkurt <Hakan.Akkurt@brenntag.de>
2021-09-01 16:28:25 +0200

    Detect Lines in Video

--- FindingLanes.py
+++ FindingLanes.py
@@ -43,7 +43,6 @@ def averageSlopeIntercept(image, lines):
         averagedLines = [rightLine]
         return averagedLines
 
-
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
